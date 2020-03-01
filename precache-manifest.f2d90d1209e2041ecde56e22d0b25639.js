bdd17958bbd666c7e9b8c0a303c4aa27e6b1fa9a
PerkinJ <perkin1221@gmail.com>
2020-03-01 17:50:55 +0800

    Updates

---
+++ precache-manifest.f2d90d1209e2041ecde56e22d0b25639.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "772271149a1fbe1bbb6eda94cc58109c",
+    "url": "/regraph-demo/index.html"
+  },
+  {
+    "revision": "1f86b1331e935ebe8b9a",
+    "url": "/regraph-demo/static/css/2.74e1a1f8.chunk.css"
+  },
+  {
+    "revision": "5a5148c57c84998d52e3",
+    "url": "/regraph-demo/static/css/main.7a338e7c.chunk.css"
+  },
+  {
+    "revision": "1f86b1331e935ebe8b9a",
+    "url": "/regraph-demo/static/js/2.aaf2f18a.chunk.js"
+  },
+  {
+    "revision": "c2c9beed0cac0f7a9365619f665e71d1",
+    "url": "/regraph-demo/static/js/2.aaf2f18a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a5148c57c84998d52e3",
+    "url": "/regraph-demo/static/js/main.c4b3814f.chunk.js"
+  },
+  {
+    "revision": "282a42ce0f2aae637615",
+    "url": "/regraph-demo/static/js/runtime-main.499d57d8.js"
+  }
+]);
