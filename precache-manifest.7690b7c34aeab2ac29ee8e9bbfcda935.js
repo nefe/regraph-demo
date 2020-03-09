7d3e200497d55d28c02cb624c6fc7ec24fb49097
PerkinJ <perkin1221@gmail.com>
2020-03-09 17:56:56 +0800

    Updates

--- precache-manifest.7690b7c34aeab2ac29ee8e9bbfcda935.js
+++ precache-manifest.7690b7c34aeab2ac29ee8e9bbfcda935.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ea8abdf3537617ae867eec969e68623a",
+    "revision": "5e1efa8e03c9f7e6cafacfa066fbaa07",
     "url": "/regraph-demo/index.html"
   },
   {
-    "revision": "7769583f39cbea1d70c6",
+    "revision": "15ca6cbb4c8dd8c2914a",
     "url": "/regraph-demo/static/css/2.74e1a1f8.chunk.css"
   },
   {
-    "revision": "861bd7bd456097505134",
+    "revision": "1453521b0b067cfc9951",
     "url": "/regraph-demo/static/css/main.17626b46.chunk.css"
   },
   {
-    "revision": "7769583f39cbea1d70c6",
-    "url": "/regraph-demo/static/js/2.4ae02b6b.chunk.js"
+    "revision": "15ca6cbb4c8dd8c2914a",
+    "url": "/regraph-demo/static/js/2.99d2a363.chunk.js"
   },
   {
     "revision": "c2c9beed0cac0f7a9365619f665e71d1",
-    "url": "/regraph-demo/static/js/2.4ae02b6b.chunk.js.LICENSE.txt"
+    "url": "/regraph-demo/static/js/2.99d2a363.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "861bd7bd456097505134",
-    "url": "/regraph-demo/static/js/main.3803b782.chunk.js"
+    "revision": "1453521b0b067cfc9951",
+    "url": "/regraph-demo/static/js/main.5e82b298.chunk.js"
   },
   {
     "revision": "282a42ce0f2aae637615",
