00694b525d2d8d83888475ef2163a393b7f913f5
PerkinJ <perkin1221@gmail.com>
2020-03-13 13:31:39 +0800

    fix: fix route basename

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import App from "./App";
 
 const rootElement = document.getElementById("root");
 render(
-  <BrowserRouter>
+  <BrowserRouter basename="regraph-demo">
     <App />
   </BrowserRouter>,
   rootElement
